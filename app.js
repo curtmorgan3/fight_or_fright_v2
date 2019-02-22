9e3c2ae8d40897b362102561ff0f4d62628b564d
curtmorgan3 <curtmorgan3@gmail.com>
2019-02-22 15:46:44 -0500

    Helper class

---
+++ app.js
@@ -0,0 +1,6 @@
+let Helper = require('./helper.js');
+
+let helper = new Helper();
+
+let num = helper.randNumber(2);
+console.log(num);
---
+++ helper.js
@@ -0,0 +1,18 @@
+class Helper{
+
+	log() {
+		console.log("log");
+	}
+
+	randNumber(n){
+		return Math.floor(Math.random() * Math.floor(n) + 1);
+	}
+
+
+
+
+
+
+
+}
+module.exports = Helper
